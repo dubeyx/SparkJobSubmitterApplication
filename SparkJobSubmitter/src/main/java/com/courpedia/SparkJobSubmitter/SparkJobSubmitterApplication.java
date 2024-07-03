39ab08dd7daeff4fd49807776a805f02c7c697c5
dubeyx <94775233+dubeyx@users.noreply.github.com>
2024-07-03 23:00:02 +0530

    first commit

---
+++ SparkJobSubmitter/src/main/java/com/courpedia/SparkJobSubmitter/SparkJobSubmitterApplication.java
@@ -0,0 +1,13 @@
+package com.courpedia.SparkJobSubmitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkJobSubmitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkJobSubmitterApplication.class, args);
+	}
+
+}
---
+++ SparkJobSubmitter/src/test/java/com/courpedia/SparkJobSubmitter/SparkJobSubmitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courpedia.SparkJobSubmitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkJobSubmitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
